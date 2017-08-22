d65aec49e025322f5c11d4235f3ad09c9f7d421c
jwats287 <jwats287@users.noreply.github.com>
2017-08-21 22:12:10 -0400

    3. Integer Skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -2,9 +2,9 @@ a = true
 
 b = false
 
-puts c = a && b
+puts a_and_b = a && b
 
-puts d = a || b
+puts a_or_b = a || b
 
 
 silly_nilly = nil
@@ -13,3 +13,12 @@ puts silly_nilly
 stilly_string = "#{silly_nilly} was here"
 print stilly_string
 puts stilly_string.class
+
+d = 3
+puts d
+
+e = 3.1
+puts e
+
+d = 3
+puts d 
